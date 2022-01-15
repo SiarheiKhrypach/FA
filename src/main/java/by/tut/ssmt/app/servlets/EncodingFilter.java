68f47e35580d018298b78beb4476b263ad62bcaa
unknown <ssmt@tut.by>
2022-01-15 07:38:55 -0800

    utf-8 encoding filter introduced

---
+++ src/main/java/by/tut/ssmt/app/servlets/EncodingFilter.java
@@ -0,0 +1,30 @@
+package by.tut.ssmt.app.servlets;
+
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter(
+        urlPatterns = "/*",
+        filterName = "EncodingFilter"
+)
+
+public class EncodingFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        servletRequest.setCharacterEncoding("UTF-8");
+        servletResponse.setCharacterEncoding("UTF-8");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
