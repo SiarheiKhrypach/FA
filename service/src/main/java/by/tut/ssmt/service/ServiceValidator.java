67c93e3a294425423acba369041192fcbe9bd41e
ssmt <ssmt@tut.by>
2023-02-09 21:03:52 +0300

    deleted commented data, isNotZero method exception set to ServiceException

--- service/src/main/java/by/tut/ssmt/service/ServiceValidator.java
+++ service/src/main/java/by/tut/ssmt/service/ServiceValidator.java
@@ -1,69 +1,27 @@
 package by.tut.ssmt.service;
 
 import by.tut.ssmt.service.exception.NullOrEmptyException;
+import by.tut.ssmt.service.exception.ServiceException;
 
 import static java.util.Objects.isNull;
 
 public class ServiceValidator {
 
-//    public void isNotNull(final String str) {
-//        if (isNull(str)) {
-//            throw new NullPointerException("The incoming string is null");
-////            throw new ControllerException ("The incoming string is null");
-//        }
-//    }
-
-public void isNotZero(final int i) {
+public void isNotZero(final int i) throws ServiceException {
         if(i == 0) {
-            throw new NullPointerException("The incoming int is 0");
+            throw new ServiceException("The incoming int is 0");
         }
 }
 
-//    public void isNotNull(final List list) {
-//        if (isNull(list)) {
-//            throw new NullPointerException("The incoming list is null");
-//        }
-//    }
-
     public <T> void isNotNull(final T t) throws NullPointerException {
-//    public <T> void isNotNull(final T t) throws ServiceException {
-//        if (t == "") {
         if (isNull(t)) {
-//            throw (new NullOrEmptyException("The incoming " + t.getClass().getName() + " list is null"));
-//            throw new ServiceException("The incoming  data is null");
-            throw new NullPointerException("The incoming  data is null");
+            throw new NullPointerException("The incoming data is null");
         }
     }
-
-//    public void isNotNull(final MenuItem menuItem) {
-//        if (isNull(menuItem)) {
-//            throw new NullPointerException("The incoming menuitem is null");
-//        }
-//    }
-
-//    public void isNotNull(final Page page) {
-//        if (isNull(page)) {
-//            throw new NullPointerException("The incoming page is null");
-//        }
-//    }
-
-//    public void isNotNull(final Product product) {
-//        if (isNull(product)) {
-//            throw new NullPointerException("The incoming product is null");
-//        }
-//    }
-
-//    public void isNotNull(final User user) {
-//        if (isNull(user)) {
-//            throw new NullPointerException("The incoming user is null");
-//        }
-//    }
-
 
     public void isNotNullOrEmpty(String...names) throws NullOrEmptyException {
         for (String name: names) {
             if (isNull(name) || (name.trim()).isEmpty()) {
-//        if (isNull(s) || "".equals(s.trim())) {
                 throw new NullOrEmptyException("The incoming string is null or empty");
             }
         }
