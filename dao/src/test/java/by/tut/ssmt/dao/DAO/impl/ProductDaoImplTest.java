70f93a5e4afa0d1aae7e3fa642b09fcef31b6983
ssmt <ssmt@tut.by>
2023-09-24 10:33:12 +0300

    Tests under construction part11

--- dao/src/test/java/by/tut/ssmt/dao/DAO/impl/ProductDaoImplTest.java
+++ dao/src/test/java/by/tut/ssmt/dao/DAO/impl/ProductDaoImplTest.java
@@ -96,15 +96,105 @@ public class ProductDaoImplTest {
         Mockito.verify(resultSet, Mockito.times(1)).getDouble(4);
         Mockito.verify(resultSet, Mockito.times(1)).getInt(5);
 
+    }
+
+    @Test
+    public void testSelectOneProductDao() throws DaoException, SQLException {
+        int testId = 1000;
+        String query = "SELECT * FROM products WHERE product_id=?";
+
+        ConnectionPool connectionPool = Mockito.mock(ConnectionPool.class);
+        Connection connection = Mockito.mock(Connection.class);
+        PreparedStatement preparedStatement = Mockito.mock(PreparedStatement.class);
+        ResultSet resultSet = Mockito.mock(ResultSet.class);
+
+        Mockito.when(connectionPool.take()).thenReturn(connection);
+        Mockito.when(connection.prepareStatement(query)).thenReturn(preparedStatement);
+        Mockito.when(preparedStatement.executeQuery()).thenReturn(resultSet);
+        Mockito.when(resultSet.next()).thenReturn(Boolean.TRUE, Boolean.FALSE);
+        Mockito.when(resultSet.getLong(1)).thenReturn(1L);
+
+        ProductDao testProductDao = new ProductDaoImpl(connectionPool);
+        testProductDao.selectOneProductDao(testId);
+
+        Mockito.verify(connectionPool, Mockito.times(1)).take();
+        Mockito.verify(connection).prepareStatement(query);
+        Mockito.verify(preparedStatement, Mockito.times(1)).executeQuery();
+        Mockito.verify(resultSet, Mockito.times(1)).next();
+        Mockito.verify(resultSet, Mockito.times(1)).getInt(1);
+        Mockito.verify(resultSet, Mockito.times(1)).getString(2);
+        Mockito.verify(resultSet, Mockito.times(1)).getInt(3);
+        Mockito.verify(resultSet, Mockito.times(1)).getInt(4);
+        Mockito.verify(resultSet, Mockito.times(1)).getInt(5);
+
 
     }
 
     @Test
-    public void testSelectOneProductDao() {
-        int testId = 1000;
-        String query = "SELECT * FROM products WHERE product_id=?";
+    public void testInsertProductDao() throws DaoException, SQLException {
+        Product testProduct = new Product("testProduct", 34, 435, 1);
+        String query = "SELECT * FROM products WHERE product_name=?";
+        String query2 = "INSERT INTO products (product_name, omega_three, omega_six, portions) Values (?, ?, ?, ?)";
+
         ConnectionPool connectionPool = Mockito.mock(ConnectionPool.class);
+        Connection connection = Mockito.mock(Connection.class);
+        PreparedStatement preparedStatement = Mockito.mock(PreparedStatement.class);
+        ResultSet resultSet = Mockito.mock(ResultSet.class);
+
+        Mockito.when(connectionPool.take()).thenReturn(connection);
+        Mockito.when(connection.prepareStatement(query)).thenReturn(preparedStatement);
+//        Mockito.doNothing().when(preparedStatement).setString(1, String.valueOf(testProduct));
+        Mockito.when(preparedStatement.executeQuery()).thenReturn(resultSet);
+        Mockito.when(resultSet.next()).thenReturn(Boolean.FALSE);
+
+        Mockito.when(connection.prepareStatement(query2)).thenReturn(preparedStatement);
+        Mockito.doNothing().when(preparedStatement).setString(1, testProduct.getProductName());
+        Mockito.doNothing().when(preparedStatement).setDouble(2, testProduct.getOmegaThree());
+        Mockito.doNothing().when(preparedStatement).setDouble(3, testProduct.getOmegaSix());
+        Mockito.doNothing().when(preparedStatement).setInt(4, testProduct.getPortions());
+        Mockito.when(preparedStatement.executeUpdate()).thenReturn(1);
+        Mockito.doNothing().when(connectionPool).retrieve(connection);
+
+        ProductDao testProductDao = new ProductDaoImpl(connectionPool);
+        testProductDao.insertProductDao(testProduct);
+
+        Mockito.verify(connectionPool).take();
+        Mockito.verify(connection).prepareStatement(query2);
+        Mockito.verify(preparedStatement).executeQuery();
+        Mockito.verify(resultSet, Mockito.times(1)).next();
+
+        Mockito.verify(connection).prepareStatement(query);
+        Mockito.verify(preparedStatement, Mockito.times(2)).setString(1, testProduct.getProductName());
+        Mockito.verify(preparedStatement).setDouble(2, testProduct.getOmegaThree());
+        Mockito.verify(preparedStatement).setDouble(3, testProduct.getOmegaSix());
+        Mockito.verify(preparedStatement).executeUpdate();
+        Mockito.verify(connectionPool).retrieve(connection);
+    }
+
+    @Test
+    public void testUpdateProductDao() throws DaoException, SQLException {
+        Product testProduct = new Product("testProduct", 34, 435, 1);
+        String query = "SELECT * FROM products WHERE product_name=? AND NOT product_id=?";
+        String query2 = "UPDATE products SET product_name = ?, omega_three = ?, omega_six = ? WHERE product_id = ?";
+
+        ConnectionPool connectionPool = Mockito.mock(ConnectionPool.class);
+        Connection connection = Mockito.mock(Connection.class);
+        PreparedStatement preparedStatement = Mockito.mock(PreparedStatement.class);
+        ResultSet resultSet = Mockito.mock(ResultSet.class);
+
+        Mockito.when(connectionPool.take()).thenReturn(connection);
+        Mockito.when(connection.prepareStatement(query)).thenReturn(preparedStatement);
+        Mockito.when(preparedStatement.executeQuery()).thenReturn(resultSet);
+        Mockito.when(resultSet.next()).thenReturn(Boolean.FALSE);
+
+
+
+        ProductDao testProductDao = new ProductDaoImpl(connectionPool);
+        testProductDao.updateProductDao(testProduct);
 
     }
 
+    @Test
+    public void testDeleteProductDao() {
+    }
 }
